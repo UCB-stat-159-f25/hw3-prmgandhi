fa0ff22e286ebcfae60037394aad5282e6181edf
prmgandhi <2902gandhiparam@gmail.com>
2025-11-06 07:36:08 +0000

    added Binder tag to readme

--- ligotools/tests/test_utils.py
+++ ligotools/tests/test_utils.py
@@ -2,21 +2,23 @@ from ligotools import utils
 import pytest
 import numpy as np
 from scipy.io import wavfile
+import tempfile
 
 def test_write_wavfile_length_and_max():
     fs = 44100
     t = np.linspace(0, 1, fs, endpoint=False)
     data = 0.5 * np.sin(2 * np.pi * 440 * t)
-    filename = "test.wav"
-    utils.write_wavfile(str(filename), fs, data)
+    with tempfile.NamedTemporaryFile(suffix=".wav") as tmpfile:
+        filename = tmpfile.name
+        utils.write_wavfile(str(filename), fs, data)
 
-    fs_read, data_read = wavfile.read(filename)
+        fs_read, data_read = wavfile.read(filename)
 
-    assert fs_read == fs
-    assert len(data_read) == len(data)
+        assert fs_read == fs
+        assert len(data_read) == len(data)
 
-    max_val = np.max(np.abs(data_read))
-    assert np.isclose(max_val, int(32767 * 0.9), atol=1)
+        max_val = np.max(np.abs(data_read))
+        assert np.isclose(max_val, int(32767 * 0.9), atol=1)
 
 
 def test_reqshift_basic_properties():
